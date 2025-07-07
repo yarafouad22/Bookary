ab2e27396ae3644b6fbff2f1f75bd3c1a048863c
Yara Fouad <yara.fouad@isoft.ae>
2025-07-07 14:46:17 +0300

    fix: remove missing chatApi import

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,14 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { booksApi } from "../Services/books";
-import { chatApi } from "../Services/chatApi";
 
 export const store = configureStore({
   reducer: {
     [booksApi.reducerPath]: booksApi.reducer,
-    [chatApi.reducerPath]: chatApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(booksApi.middleware)
-      .concat(chatApi.middleware),
+    getDefaultMiddleware().concat(booksApi.middleware),
 });
