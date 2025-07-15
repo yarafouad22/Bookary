addbce2f7b8d6a9cff215cc385c37a8daf295b25
Yara Fouad <yara.fouad@isoft.ae>
2025-07-15 14:45:11 +0300

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          dayjs: ["dayjs"],
-        },
-      },
-    },
-  },
 });
